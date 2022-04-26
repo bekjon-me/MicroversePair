6c37868a941b9c53681efa3a0e492e40f3f5a1b1
Bekjon Ishpulatov <84561206+bekjon-me@users.noreply.github.com>
2022-04-26 20:18:18 +0500

    all tests passed

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 // !! IMPORTANT README:
 
-// You may add additional external JS and CSS as needed to complete the project, however the current external resource MUST remain in place for the tests to work. BABEL must also be left in place. 
+// You may add additional external JS and CSS as needed to complete the project, however the current external resource MUST remain in place for the tests to work. BABEL must also be left in place.
 
 /***********
 INSTRUCTIONS:
@@ -24,5 +24,11 @@ INSTRUCTIONS:
 
 // PLEASE NOTE: Adding global style rules using the * selector, or by adding rules to body {..} or html {..}, or to all elements within body or html, i.e. h1 {..}, has the potential to pollute the test suite's CSS. Try adding: * { color: red }, for a quick example!
 
-// Once you have read the above messages, you can delete all comments. 
+// Once you have read the above messages, you can delete all comments.
 "use strict";
+
+let submit = document.getElementById("submit");
+
+submit.addEventListener("click", () => {
+  open(" https://www.freecodecamp.com/email-submit");
+});
