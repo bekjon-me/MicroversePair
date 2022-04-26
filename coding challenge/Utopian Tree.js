9db760d88d91fc35d1516d716f77c7de2817cd8e
Codeaddictjaver <hatimeddinani@gmail.com>
2022-04-26 18:40:58 +0300

    Utopia Tree

---
+++ coding challenge/Utopian Tree.js
@@ -0,0 +1,15 @@
+function utopianTree(n) {
+    let height = 1;
+    let cycle = 1;
+    for ( let i = 0; i < n; i++){
+        if(cycle % 2 === 0){
+            height *= 2;
+        }   else {
+            height ++;
+        } 
+    }
+       
+         
+    return height ;
+
+}
